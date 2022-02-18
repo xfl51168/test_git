cefe940878079d0d24ee007a0dee3c427441f71c
xfl <419459550@qq.com>
2022-02-18 17:01:10 +0800

    test_git

---
+++ src/com/xfl/Hello.java
@@ -0,0 +1,13 @@
+package com.xfl;
+
+/**
+ * @author xfl
+ * @version 1.0
+ * @create 2022-01-27 11:20
+ * @describe
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
